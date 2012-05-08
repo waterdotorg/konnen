bc01640dc101bf45b6be83d2b6c5b8666bf02cbe
Vince Carney <vincecarney@gmail.com>
2012-05-08 15:29:34 -0500

    Sms dev updates

--- project/sms/management/commands/daemon_sms_get_messages.py
+++ project/sms/management/commands/daemon_sms_get_messages.py
@@ -1,6 +1,8 @@
 import logging
 import time
 
+import sms
+
 from django import db
 from django.core.management.base import BaseCommand, CommandError
 
@@ -8,16 +10,15 @@ from django.core.management.base import BaseCommand, CommandError
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = 'Digicel Inbox Polling'
+    help = 'Sms Message Polling'
     args = ''
 
     def handle(self, *args, **options):
-        i = 0
         while True:
-            if i > 10:
-                logger.error('i is > 10 dude! sys.exit() confirmed!!!')
-                raise Exception('My exception raised')
-            i += 1
+            try:
+                s = sms.get_messages()
+            except Exception, e:
+                logger.error("Error importing sms messages: %s" % e)
             # TODO: test memory leakage with DEBUG=False. Uncomment reset_queries() if memory leakage present.
             #db.reset_queries()
             time.sleep(5)
